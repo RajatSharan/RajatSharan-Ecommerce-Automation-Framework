b984e9f68217ae4cc7e7843423af18d129c55b09
Rajat Sharan <errajatsharan@gmail.com>
2023-10-31 18:53:58 +0530

    GetByLocator

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -6,6 +6,14 @@ test('Playwright special Character',async ({page})=>{
     await page.getByLabel("Check me out if you Love IceCreams!").click()
     await page.getByLabel("Employed").click()
     await page.getByLabel("Gender").selectOption("Female")
+    await page.getByPlaceholder("Password").fill("abc123")
+    await page.getByRole("button",{name: 'Submit'}).click();
+    await page.getByText(" Success! The Form has been submitted successfully!.").isVisible()
+    await page.getByRole("link",{name :"Shop"}).click()
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click()
+
+
+
 
 
 })

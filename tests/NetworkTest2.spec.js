41815a18516315cfb1cee4121cdfbc09e480cfa7
Rajat Sharan <errajatsharan@gmail.com>
2023-11-05 01:41:47 +0530

    intercept Network request calls with Playwright

---
+++ tests/NetworkTest2.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require("@playwright/test");
+
+test("Security test request intercept", async ({ page }) => {
+  //login and reach the order page
+  const email = "testrajat34@gmail.com";
+  await page.goto("http://rahulshettyacademy.com/client");
+  await page.locator("#userEmail").fill("testrajat34@gmail.com");
+  await page.locator("#userPassword").fill("Rajat@25");
+  //Race condition
+  await Promise.all([page.waitForNavigation(), page.locator("#login").click()]);
+  await page.waitForLoadState("networkidle");
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.route(
+    "https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*",
+    (route) =>
+      route.continue({
+        url: "https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=654690e77244490f95df4465 ",
+      })
+  );
+  await page.locator("button:has-text('View')").first().click();
+  await expect(page.locator("p").last()).toHaveText("You are not authorize to view this order")
+});

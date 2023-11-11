56bc1e0926efef99e7d118b6884fb5e3d9f1e643
Rajat Sharan <errajatsharan@gmail.com>
2023-11-12 01:10:55 +0530

    drive the data from external json files

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,24 +1,21 @@
 const {test,expect}=require('@playwright/test')
 import POManger from '../pageobjects/POManager'
-//const {POManger} = require('../pageobjects/POManager')
+const dataset=JSON.parse(JSON.stringify(require('./utils/ClientAppPOTestData.json')))
 
 
 test.only("Login Functionality",async({page})=>
 
 {
 const poManager= new POManger(page)
-const username = 'testrajat34@gmail.com'
-const password = "Rajat@25"
-const productName='adidas original';
 const products =page.locator(".card-body");
 const loginPage= poManager.getLoginPage()
 await loginPage.goTo()
-await loginPage.validLogin(username,password)
+await loginPage.validLogin(dataset.username,dataset.password)
 const dashboardPage = poManager.getdashboardPage()
-await dashboardPage.searchProductAddCart(productName)
+await dashboardPage.searchProductAddCart(dataset.productName)
 await dashboardPage.navigateToCart()
 const cartPage = poManager.getCartPage();
-await cartPage.VerifyProductDisplayed(productName);
+await cartPage.VerifyProductDisplayed(dataset.productName);
 await cartPage.Checkout();
 const ordersReviewPage = poManager.getOrdersReviewPage();
 await ordersReviewPage.searchCountryAndSelect("ind","India");
